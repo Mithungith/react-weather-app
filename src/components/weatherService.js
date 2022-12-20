4de4f68760bbad37699d8e1cb230a34bb32fa370
Mithun <mithuny1998@gmail.com>
2022-12-20 08:27:00 +0530

    changes file

---
+++ src/components/weatherService.js
@@ -0,0 +1,34 @@
+const API_KEY = "15e4deaafce1fce2cb5c7c63080e1472";
+const makeIconURL= (iconId)=>` http://openweathermap.org/img/wn/${iconId}@2x.png`
+
+const getFormattedWeatherData = async (city, units = "metric") => {
+  const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=${units}`;
+
+  const data = await fetch(URL)
+    .then((res) => res.json())
+    .then((data) => data);
+
+    const {
+        weather,
+        main:{temp,feels_like,temp_min,temp_max,pressure,humidity},
+        wind:{speed},
+        sys:{country},
+        name,
+    }= data;
+    const {description,icon} = weather[0];
+    return {
+        description,
+        iconURL:makeIconURL(icon),
+        temp,
+        feels_like,
+        temp_min,
+        temp_max,
+        pressure,
+        humidity,
+        speed,
+        country,
+        name
+    }
+};
+
+export {getFormattedWeatherData};
